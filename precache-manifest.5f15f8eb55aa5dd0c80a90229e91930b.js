df61a246dbba60bb2184e735b77fa40e06b447fe
Zenen <zenen52@gmail.com>
2020-08-20 22:56:55 -0700

    Updates

---
+++ precache-manifest.5f15f8eb55aa5dd0c80a90229e91930b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65d84a485655fae0f3b904a3e063c19",
+    "url": "/Freebook/index.html"
+  },
+  {
+    "revision": "7ecedbd2da32add91461",
+    "url": "/Freebook/static/css/2.98f886d0.chunk.css"
+  },
+  {
+    "revision": "2a10d47dc32999733558",
+    "url": "/Freebook/static/css/main.2cdc5a66.chunk.css"
+  },
+  {
+    "revision": "7ecedbd2da32add91461",
+    "url": "/Freebook/static/js/2.d21613ae.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Freebook/static/js/2.d21613ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a10d47dc32999733558",
+    "url": "/Freebook/static/js/main.cc0e82f9.chunk.js"
+  },
+  {
+    "revision": "eccb0bca435650fac412",
+    "url": "/Freebook/static/js/runtime-main.e463d5a5.js"
+  },
+  {
+    "revision": "b3657f8632f19c2d617e6562d7894668",
+    "url": "/Freebook/static/media/banner2.b3657f86.png"
+  },
+  {
+    "revision": "99ef0b5bca4312a710b0726ba0b135df",
+    "url": "/Freebook/static/media/me_mountaintop_thumb.99ef0b5b.jpg"
+  }
+]);
