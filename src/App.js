a3dd15a4070cadc789a3044e212da7a2c22240c4
Zenen Treadwell <zenen52@gmail.com>
2020-09-06 12:10:12 -0700

    Added resume link

--- src/App.js
+++ src/App.js
@@ -31,8 +31,8 @@ class App extends React.Component {
             profile_pic,
             banner,
             links: [
-                { id: 1, name: "Github", addr: "https://github.com/ZenenTreadwell/"},
-                { id: 2, name: "Resume", addr: "https://zenentreadwell.github.io/Freebook/resume/"},
+                { id: 1, name: "Resume", addr: "https://zenentreadwell.github.io/Freebook/resume/"},
+                { id: 2, name: "Github", addr: "https://github.com/ZenenTreadwell/"},
                 { id: 3, name: "LinkedIn", addr: "https://www.linkedin.com/in/zenen-treadwell/"},
                 { id: 4, name: "Instagram", addr: "https://www.instagram.com/zenentreadwell/"},
                 { id: 5, name: "Reddit", addr: "https://www.reddit.com/user/Zenen/"},
